06da20877ed3a0eec020c3be79c58eb7a3268287
Iriel <38636314+irielr@users.noreply.github.com>
2020-05-08 11:20:17 +0200

    Add files via upload

--- www/js/mimaps.js
+++ www/js/mimaps.js
@@ -15,8 +15,8 @@ var app = {
 		var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
 		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXJpZWwiLCJhIjoiY2s5d3dwNDB2MDVhODNlcWRrd3l1djR4aSJ9.AvRXaGoKZIzNNVpxRetaKA', {
-			attribution: '',//Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			maxZoom: 18	}).addTo(miMapa);
+			attribution: '',//Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imag© <a href="http://mapbox.com">Mapbox</a>',
+			maxZoom: 13	}).addTo(miMapa);
 
 		app.pintaMarcador([position.coords.latitude, position.coords.longitude], '¡Estoy aquí!', miMapa);
 
